9511c2a15540a5b33110fac0eb7a2c84772c211a
Alex Lerner <alernerdev@gmail.com>
2017-12-20 17:05:36 -0500

    converted upload python script int a lambda function

--- upload-portfolio.lambda.py
+++ upload-portfolio.lambda.py
@@ -4,19 +4,24 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.pragmaticbitbucket.com')
-build_bucket = s3.Bucket('portfoliobuild.pragmaticbitbucket.com')
+    s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
 
-# download zip file into the stream
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.pragmaticbitbucket.com')
+    build_bucket = s3.Bucket('portfoliobuild.pragmaticbitbucket.com')
 
-# and upload each file in the zip into the bucket
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        # make the uploaded file public readable
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    # download zip file into the stream
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
+
+    # and upload each file in the zip into the bucket
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+            # make the uploaded file public readable
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+    print "Portfolio job done!"
+    return "Hello from Lambda"
